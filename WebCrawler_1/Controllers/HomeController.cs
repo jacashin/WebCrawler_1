4698e8977cb6e8faada8245a2e278f3f13b8ba50
Jacashin <jacobashin@gmail.com>
2021-12-14 23:01:36 -0500

    clean up

--- WebCrawler_1/Controllers/HomeController.cs
+++ WebCrawler_1/Controllers/HomeController.cs
@@ -261,21 +261,7 @@ namespace WebCrawler_1.Controllers
             }
             return View("Index", docPath);
         }
-        public static async Task Timer()
-        {
-            // Grab the Scheduler instance from the Factory
-            StdSchedulerFactory factory = new StdSchedulerFactory();
-            IScheduler scheduler = await factory.GetScheduler();
 
-            // and start it off
-            await scheduler.Start();
-
-            // some sleep to show what's happening
-            await Task.Delay(TimeSpan.FromSeconds(10));
-
-            // and last shut down the scheduler when you are ready to close your program
-            await scheduler.Shutdown();
-        }
     }
 }
 
